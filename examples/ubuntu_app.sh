eac25255eedbaad092e4871575d85bbd09e59a5c
msuad <1445617868@qq.com>
2025-12-03 17:10:36 +0800

    examples：添加了Ubuntu（flatpak）初始化下载的app

---
+++ examples/ubuntu_app.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-modify flathub --url=https://mirrors.ustc.edu.cn/flathub
+sudo apt install curl git flatpak gnome-software-plugin-flatpak vim wget openssh-server -y
+sudo flatpak install com.microsoft.Edge com.qq.QQ  com.tencent.WeChat  org.telegram.desktop com.discordapp.Discord com.tencent.wemeet com.qq.QQmusic
