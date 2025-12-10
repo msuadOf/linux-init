239d602bbfb836c8b14cfeea6faa04c8af60d5fc
白逸凡 <57826721+msuadOf@users.noreply.github.com>
2025-12-10 20:33:26 +0800

    add: DistroShelf

--- examples/ubuntu_app.sh
+++ examples/ubuntu_app.sh
@@ -2,4 +2,5 @@
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-modify flathub --url=https://mirrors.ustc.edu.cn/flathub
 sudo apt install curl git flatpak gnome-software-plugin-flatpak vim wget openssh-server -y
-sudo flatpak install com.microsoft.Edge com.qq.QQ  com.tencent.WeChat  org.telegram.desktop com.discordapp.Discord com.tencent.wemeet com.qq.QQmusic
+sudo flatpak install com.microsoft.Edge com.qq.QQ  com.tencent.WeChat  org.telegram.desktop \
+  com.discordapp.Discord com.tencent.wemeet com.qq.QQmusic com.ranfdev.DistroShelf
